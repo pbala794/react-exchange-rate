7e021f95d64989e4938b13b4d82570715fc7bb99
piotr794 <panbala513@gmail.com>
2018-03-26 19:24:59 +0000

    TransactionList component styling

--- src/components/TransactionList/TransactionList.js
+++ src/components/TransactionList/TransactionList.js
@@ -3,23 +3,28 @@ import React, { Component } from 'react';
 import './TransactionList.css';
 
 class TransactionList extends Component {
-    
-    render() {
+  
+  render() {
+      
+      return (
         
-        return (
-          
-          <div>
-            <ul className="transaction-list">
-                <li>Transakcja 1</li>
-                <li>Transakcja 2</li>
-                <li>Transakcja 3</li>
-            </ul>
+        <div>
+          <h3>Historia Twoich transakcji</h3>
+          <div className="top-transaction-wrapper">
+            <p>Transakcja z najwyższą kwotą: </p>
+            <span>Zakup samochodu: 75000</span>
           </div>
-                
-        );
-        
-    }
-    
+          <ul className="transaction-list">
+            <li>Transakcja 1</li>
+            <li>Transakcja 2</li>
+            <li>Transakcja 3</li>
+          </ul>
+        </div>
+              
+      );
+      
+  }
+  
 }
 
 export default TransactionList;
