50fcb64c25f306bba8e6bd3af67ff577122c484f
piotr794 <panbala513@gmail.com>
2018-03-27 14:08:48 +0000

    lifting state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,10 +6,29 @@ import TransactionList from '../TransactionList/TransactionList';
 import './App.css';
 
 class App extends Component {
+    
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+        currencyRate: 0
+      };
+      this.handleEurRateInput = this.handleEurRateInput.bind(this);
+      
+    }
+    
+    handleEurRateInput(inputValue) {
+      this.setState({currencyRate: inputValue}, () => {
+          console.log(this.state);
+      });
+    }
+  
     render() {
+      // const currencyRate = this.state.currencyRate;
+      
         return(
           <div className="app-container">
-            <CurrencyTransaction />
+            <CurrencyTransaction onInputChange={ this.handleEurRateInput } />
             <TransactionList />
           </div>
         );
--- src/components/CurrencyTransation/CurrencyTransaction.js
+++ src/components/CurrencyTransation/CurrencyTransaction.js
@@ -3,6 +3,16 @@ import './CurrencyTransaction.css';
 
 class CurrencyTransaction extends Component {
     
+    constructor(props) {
+        super(props);
+        
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+    
+    handleInputChange(e) {
+        this.props.onInputChange(e.target.value);
+    }
+    
     render() {
         return (
           <div>
@@ -10,7 +20,7 @@ class CurrencyTransaction extends Component {
             <h2>Podaj kurs euro:</h2>
             <span className="eur-input">
                 <img src="img/euro.svg" alt="eur-icon" />
-                <input type="text" id="rate" placeholder="1 EUR = 0.00 PLN" pattern="\d+(.\d)?" />
+                <input type="text" id="rate" placeholder="1 EUR = 0.00 PLN" pattern="\d+(.\d)?" onChange={ this.handleInputChange } />
             </span>
             
             <div className="transaction-content">
@@ -18,8 +28,17 @@ class CurrencyTransaction extends Component {
                <form>
                     <label for="name">Nazwa transakcji</label>
                     <input type="text" id="name" />
-                    <label for="amoun">Kwota (EUR)</label>
-                    <input type="text" id="amount" />
+                    <div className="exchange-box">
+                        <div>
+                            <label for="amount-eur">EUR</label>
+                            <input type="text" id="amount" />
+                        </div>
+                        <img src="img/euro.svg" alt="eur-icon" />
+                        <div>
+                            <label for="amount-pln">PLN</label>
+                            <input type="text" id="amount" />
+                        </div>
+                    </div>
                </form>
            </div>
            
